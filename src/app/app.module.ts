311b8d22b24b81b69efb66adb8a35cd58cc06b5c
Andres Tafur <luis.tafur@aguayo.co>
2022-10-27 10:41:49 -0500

    Feature(Animation of pages) - Config for change the animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+// Custom native page transition
+import { NativePageTransitions } from "@awesome-cordova-plugins/native-page-transitions/ngx";
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -10,7 +13,7 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, NativePageTransitions],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/pages/welcome/welcome.module.ts
+++ src/app/pages/welcome/welcome.module.ts
@@ -1,3 +1,7 @@
+// Custom native page transition
+import { NavController, Platform } from '@ionic/angular';
+import { NativePageTransitions, NativeTransitionOptions } from '@awesome-cordova-plugins/native-page-transitions/ngx';
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -17,4 +21,39 @@ import { WelcomePage } from './welcome.page';
   ],
   declarations: [WelcomePage]
 })
-export class WelcomePageModule {}
+
+export class WelcomePageModule {
+  constructor(public navCtrl: NavController, private nativePageTransitions: NativePageTransitions, private platform: Platform) {
+    this.ionViewWillLeave();
+  }
+
+  //Este es un ejemplo de salida de página
+  ionViewWillLeave() {
+    let options: NativeTransitionOptions = {
+      direction: 'down',
+      duration: 500,
+      slowdownfactor: 3,
+      slidePixels: 20,
+      iosdelay: 100,
+      androiddelay: 150,
+      fixedPixelsTop: 0,
+      fixedPixelsBottom: 60
+    }
+
+    if (this.platform.is("cordova")) {
+      this.nativePageTransitions.slide(options)
+        .catch(onerror);
+    } else {
+      console.log("Run browser");
+    }
+
+  }
+
+  // Este es un ejemplo de entrada en la página
+  // openPage(page: any) {
+  //   this.nativePageTransitions.slide(this.options);
+  //   this.navCtrl.navigateForward(page);
+  //   // this.navCtrl.navigateRoot(page);
+  //   console.log("AQUI");
+  // }
+}

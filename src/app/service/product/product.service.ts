d2480b31f27f5c3b6113991b08d7d13e79a75ed7
Andres Tafur <luis.tafur@aguayo.co>
2022-11-20 19:38:36 -0500

    Feature(Productos - Home) - Consume servicio para el componente

---
+++ src/app/service/product/product.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import axios from 'axios';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  public arrayDataProducts = new Array();
+
+  constructor() { }
+
+  async getProducts() {
+    await axios.get(`${environment.apiPath}/consultarProductos`, environment.headerConfig).then(response => {
+
+      for (let index = 0; index < response.data.data.length; index++) {
+        const element = response.data.data[index];
+        this.arrayDataProducts[index] = element
+      }
+
+    })
+  }
+
+  async getRecomendedProducts(nit) {
+    await axios.get(`${environment.apiPath}/getRecommended?nit=${nit}`, environment.headerConfig).then(response => {
+
+      for (let index = 0; index < response.data.data.length; index++) {
+        const element = response.data.data[index];
+        this.arrayDataProducts[index] = element
+      }
+
+    })
+  }
+
+  async getCurrentProducts(nit) {
+    // let session = true;
+    let session = false;
+
+    if (!session) {
+      return await this.getProducts()
+    } else {
+      return await this.getRecomendedProducts("99313")
+    }
+  }
+
+  public getArrayDataProducts() {
+    return this.arrayDataProducts
+  }
+
+}

e78665d68d224671f85cee17e549b2f4e4b23308
Andres Tafur <luis.tafur@aguayo.co>
2022-12-28 13:47:04 -0500

    Agrega loader en el search

--- src/app/component/layout/header/header.component.ts
+++ src/app/component/layout/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { LoadingController, NavController } from '@ionic/angular';
 import { SearchService } from 'src/app/service/search/search.service';
 import { ShopingCarService } from 'src/app/service/shoping-car.service';
 
@@ -12,10 +12,13 @@ export class HeaderComponent implements OnInit {
   public numberProductsCar;
   public productsIsNull = true;
 
+  public loader;
+
   constructor(
     public shopinCarService: ShopingCarService,
     public nvCtrl: NavController,
-    public searchService: SearchService
+    public searchService: SearchService,
+    public loadingController: LoadingController
   ) { }
 
   ngOnInit() {
@@ -40,19 +43,37 @@ export class HeaderComponent implements OnInit {
 
       if (e.keyCode === 13 && !e.shiftKey) {
         if (document.activeElement.tagName == "INPUT") {
+          this.showLoader()
 
           this.searchService.getProductsOfSearch(
             form.querySelector("input").value
           ).then(() => {
-            this.nvCtrl.navigateForward("search")
+
+            if (!window.location.pathname.includes("search")) {
+              this.nvCtrl.navigateForward("search")
+            }
+
             form.querySelector("input").value = ""
-          })
+          }).finally(() => this.removeLoader())
 
         }
       }
 
     })
 
+  }
+
+  async showLoader() {
+    this.loader = await this.loadingController.create({
+      spinner: "bubbles",
+      translucent: true,
+      cssClass: 'o-loader'
+    });
+    await this.loader.present();
+  }
+
+  async removeLoader() {
+    this.loader = await this.loadingController.dismiss();
   }
 
 }
